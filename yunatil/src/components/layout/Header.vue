2583bca54e6ebea4e3a440f7ec904c6a5f808596
yuna <yunaka.dev@gmail.com>
2022-03-30 22:33:47 +0900

    [Docs] Header 수정

--- yunatil/src/components/layout/Header.vue
+++ yunatil/src/components/layout/Header.vue
@@ -1,40 +1,34 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+<div>
+  <b-navbar class="nav_bar" toggleable="lg" variant="faded" type="light">
+    <b-navbar-brand class="title" href="#">YunaTil</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-navbar type="light" >
+            <b-form-input class="mr-sm-2" size="sm"  placeholder="Search"></b-form-input>
+            <b-button variant="outline-success" size="sm"  class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-navbar>
+
+        <b-nav-item-dropdown right>
+          <template #button-content>
+            <em>메뉴</em>
+          </template>
+          <b-dropdown-item href="#">고도몰</b-dropdown-item>
+          <b-dropdown-item href="#">Vue</b-dropdown-item>
+          <b-dropdown-item href="#">React</b-dropdown-item>
+          <b-dropdown-item href="#">Node.js</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  <div>
+    <h1 class="sub_title">Today I Learned</h1>
+    <img class="view_img" src="./../../assets/img/bg-masthead.jpg" alt="..." />
   </div>
-</nav>
+</div>
 </template>
 
 <script>
@@ -42,3 +36,23 @@ export default {
     name: "header",
 };
 </script>
+
+<style>
+.nav_bar{
+  z-index: 10;
+}
+.sub_title{
+  color: white;
+}
+.view_img{
+  width: 100%;
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  top:0;
+  z-index:1;
+}
+.title{
+  margin-left: 20px;
+}
+</style>
